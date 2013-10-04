0ac80ba95eee9f0838e91b83ea950b9995e2b8f0
Matt Hodges <hodgesmr1@gmail.com>
2013-10-04 16:22:47 -0400

    placeholder html

--- PGSServer.rb
+++ PGSServer.rb
@@ -7,6 +7,7 @@ require File.dirname(__FILE__) + '/PGSRoutes'
 
 if $0 == __FILE__ then
 	server = WEBrick::HTTPServer.new(:Port=>8000)
+	server.mount "/", WEBrick::HTTPServlet::FileHandler, './Static/'
 	server.mount "/play", PlayRoute
 	server.mount "/control", ControlsRoute
 	trap "INT" do
