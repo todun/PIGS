091f7eb90bfadc665cc4bd0adda4f41121928103
Matt Hodges <hodgesmr1@gmail.com>
2013-10-04 14:45:19 -0400

    first pieces of server

---
+++ PGSServer.rb
@@ -0,0 +1,25 @@
+require 'rubygems'
+require 'webrick'
+
+class Player < WEBrick::HTTPServlet::AbstractServlet
+	def do_PUT(request, reponse)
+		play_track(request)
+	end
+
+	def play_track(request)
+		if(requested_track = request.query['query'])
+			puts "playing track #{requested_track}"
+		end
+	end
+end
+
+if $0 == __FILE__ then
+	server = WEBrick::HTTPServer.new(:Port=>8000)
+	server.mount "/play", Player
+	trap "INT" do
+		server.shutdown
+	end
+	server.start
+end
+
+
