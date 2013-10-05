32b5825b5aba4b579f9ca47ea87ea071c52b3e7f
Matt Hodges <hodgesmr1@gmail.com>
2013-10-05 10:04:02 -0400

    stop currently playing song before starting new one

--- PGSTuner.rb
+++ PGSTuner.rb
@@ -8,6 +8,7 @@ class PGSTuner
 	def initialize
 		init_grooveshark
 		@read_io, @write_io = IO.pipe
+		@child = nil
 	end
 
 	def init_grooveshark
@@ -17,12 +18,17 @@ class PGSTuner
 	end
 
 	def play_song_for_query(query)
+		
+		if !@child.nil? then
+			execute_tuner_command("q")
+		end
+
 		query.strip!
 		songs = @grooveshark_client.search_songs(query)
 		song = songs.first
 		url = @grooveshark_client.get_song_url(song)
 
-		child = fork do
+		@child = fork do
 			STDIN.reopen(@read_io)
 			`mplayer -really-quiet "#{url}"` 
 		end
